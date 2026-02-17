b953da93b899d55cc3051ffdcb2a77b8554f17f4
sunaissu <galvezbrett17@gmail.com>
2026-02-17 16:59:50 +0800

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NoteBoard = () => {
+    return (
+        <div style={{ padding: 20, border: '1px solid #ccc' }}>
+            Welcome to noteboard
+        </div>
+    );
+};
