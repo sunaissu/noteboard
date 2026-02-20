5f655eaa2b56f0187366d907e5945a4dbb6fa092
sunaissu <galvezbrett17@gmail.com>
2026-02-20 17:42:37 +0800

    chore: setup playground for UI view testing

---
+++ playground/src/App.tsx
@@ -0,0 +1,12 @@
+import { NoteBoard } from '@sunaissu/noteboard'
+
+function App() {
+    return (
+        <div>
+            <h1>Noteboard Playground</h1>
+            <NoteBoard />
+        </div>
+    )
+}
+
+export default App
